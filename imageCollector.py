a22c6b7fbd9e01f09710ef0b84b92d5f19354343
David Li <dali7758769@gmail.com>
2021-01-13 13:49:28 -0500

    add inputs

--- imageCollector.py
+++ imageCollector.py
@@ -3,3 +3,4 @@ import bs4
 class imageCollector:
     def __init__(self, link):
         self.link = link
+
--- run.py
+++ run.py
@@ -1,4 +1,26 @@
 from imageCollector import imageCollector
 
+def getLink():
+    result = []
+
+    firstq = input('Collect images from a single link? [y/n] ').lower()
+
+    if(firstq == 'y'):
+        result.append(input('Input link: '))
+    elif(firstq == 'n'):
+        path = input('Provide path to textfile which contains links (there must be one link on each line): ')
+
+        file = open(path, 'r')
+
+        line = file.readline().replace('\n', '')
+
+        while line:
+            result.append(line)
+            line = file.readline().replace('\n', '')
+
+    return result
+
 if __name__ == '__main__':
-    print('hi')
+
+    linklist = getLink()
+
